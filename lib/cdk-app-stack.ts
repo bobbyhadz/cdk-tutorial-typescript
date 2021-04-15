05233f4e0506f09fffe089a650686436fc066182
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-15 15:20:22 +0300

    updated billing mode to PAY_PER_REQUEST

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,9 +1,19 @@
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'avatars-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const table = new dynamodb.Table(this, 'todos-table', {
+      partitionKey: {name: 'todoId', type: dynamodb.AttributeType.NUMBER},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
