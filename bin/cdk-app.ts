2aa466ed1bd849598361de3287f3b2bfab31256e
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-15 18:47:34 +0300

    added prod stack

--- bin/cdk-app.ts
+++ bin/cdk-app.ts
@@ -4,8 +4,17 @@ import 'source-map-support/register';
 import {CdkAppStack} from '../lib/cdk-app-stack';
 
 const app = new cdk.App();
+
 new CdkAppStack(app, 'cdk-stack-dev', {
   stackName: 'cdk-stack-dev',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+new CdkAppStack(app, 'cdk-stack-prod', {
+  stackName: 'cdk-stack-prod',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
